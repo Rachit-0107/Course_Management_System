c38a350f77f50d475100973d13bf330c300c4cd6
Rachit Agrawal <80877174+Rachit-0107@users.noreply.github.com>
2021-11-24 17:31:25 +0530

    Update CourseMan.java

--- CourseMan.java
+++ CourseMan.java
@@ -1,4 +1,5 @@
-import java.util.*;  
+import java.util.*;
+import java.util.concurrent.TimeUnit;
 import java.io.*;
 
 class Test{
@@ -81,7 +82,7 @@ class Course extends CourseDetails{
 	{
 		super(nameCourse,unitsCourse,Classroom); 
 		this.no_of_students = no_of_students; 
-	}
+	} 
 } 
 
 class Student{
@@ -102,32 +103,41 @@ class Student{
         this.country=country; 
         c1.nameofStudents.add(this.name); 
         c1.idNoofStudentsenrolled.add(this.idNumber);
-        try{
-			FileWriter w= new FileWriter("StudentDetails.txt", true);
+        if(!this.name.equals(""))
+        {
+        	try{
+        		FileWriter w= new FileWriter("StudentDetails.txt", true);
 
-			w.write(this.name + "\n");
-			w.write(this.idNumber + "\n");
-			w.write(this.emailAddress + "\n");
-			w.write(this.country+"\n");
-			w.close();
-		}
-		catch (Exception ae) {
-			System.out.println(ae);
-		}
+        		w.write(this.name + "\n");
+        		w.write(this.idNumber + "\n");
+        		w.write(this.emailAddress + "\n");
+        		w.write(this.country+"\n");
+        		w.close();
+        	}
+        	catch (Exception ae) {
+        		System.out.println(ae);
+        	} 
+        }
+    } 
+    public static void printWithDelays(String data, TimeUnit unit, long delay) throws InterruptedException {
+        for (char ch:data.toCharArray()) {
+            System.out.print(ch);
+            unit.sleep(delay);
+        }
     }
     
     int marksOfTest(String testName){
-         if(testName=="Quiz")
+         if(testName.equals("Quiz"))
          {
              return marks.get("Quiz");
          }
-         if(testName=="Midsemester Examination")
+         if(testName.equals("Midsemester_Examination"))
          {
-            return marks.get("Midsemester Examination");
+            return marks.get("Midsemester_Examination");
         }
-        if(testName=="Comprehensive Examination")
+        if(testName.equals("Comprehensive_Examination"))
         {
-            return marks.get("Comprehensive Examination");
+            return marks.get("Comprehensive_Examination");
         }
         else
         	return 0;
@@ -152,15 +162,15 @@ class Student{
     	}
     	return avlTest;
     }
-    String viewEnrolledCourse(){
-        return courseName;
+    void viewEnrolledCourse(){
+        System.out.println(courseName);
     }
     void viewPersonalDetails(){
         System.out.println("Name: "+name);
         System.out.println("BITS ID: "+idNumber);
         System.out.println("Email Address: "+emailAddress);
         System.out.println("Country: "+country);
-        System.out.println("Quiz: "+marks.get("Quiz")+" "+"Mid semester Examination: "+marks.get("Midsemester Examination")+" "+"Comprehensive Examination: "+marks.get("Comprehensive Examination"));
+        System.out.println("Quiz: "+marks.get("Quiz")+" "+"Midsemester_Examination: "+marks.get("Midsemester_Examination")+" "+"Comprehensive_Examination: "+marks.get("Comprehensive_Examination"));
     }
     static void allRegisteredStudents(Course c1){ 
     	for(int i=0; i<c1.nameofStudents.size(); i++)
@@ -168,12 +178,15 @@ class Student{
     		System.out.println(c1.nameofStudents.get(i)+" "+ c1.idNoofStudentsenrolled.get(i));
     	}
     } 
-    public void takeTest(Course c1)
+    public void takeTest(Course c1) throws InterruptedException
     {
     	ArrayList<String> avltest = this.availableTests(c1); 
     	Test t; 
     	ArrayList<Character> answers = new ArrayList<Character>();
     	int index=0; 
+    	System.out.print("Showing available tests");
+        Student.printWithDelays("...", TimeUnit.MILLISECONDS, 1500); 
+		System.out.println("");
     	for(int i=0; i<avltest.size();i++)
     	{
     		System.out.println(avltest.get(i));
@@ -189,12 +202,13 @@ class Student{
     				index = i; 
     			}
     		} 
-    		t = c1.allTest.get(index); 
+    		t = c1.allTest.get(index);  
+    		System.out.print("The details of the test are");
+    		Student.printWithDelays("...", TimeUnit.MILLISECONDS, 500);
     		System.out.println("The test name is " + t.testName );
     		System.out.println("The test weightage is " + t.weightagePercent );
     		System.out.println("The test duration is " + t.duration );
     		System.out.println("The test has " + t.noOfQuestions +" questions.");
-    		System.out.println("Please enter the answers for the questions");
     		System.out.println("Please enter the answers for the questions"); 
     		System.out.println("Enter a,b,c,d to answer the question or N is you don't want to attempt");
     		System.out.println("Every quetion carries +4 for correct answer and -1 for wrong answer");
@@ -208,6 +222,9 @@ class Student{
     	
     		int markgot = t.evaluatingTest(answers); 
     		marks.put(attemptTest, markgot); 
+    		System.out.print("Evaluating marks"); 
+    		Student.printWithDelays("...", TimeUnit.MILLISECONDS, 2500); 
+    		System.out.println("");
     		System.out.println("You got "+this.marks.get(t.testName)+" marks in the test you attempted"); 
     		this.attemptStatus.add(attemptTest);
     	
@@ -240,22 +257,25 @@ class Teacher{
     Map<String , Integer> viewMarks(Student st){
         Map<String, Integer> testMarks = new HashMap<String, Integer>();
         testMarks.put("Quiz",st.marks.get("Quiz"));
-        testMarks.put("Midsemester Examination",st.marks.get("Midsemester Examination"));
-        testMarks.put("Comprehensive Examination",st.marks.get("Comprehensive Examination"));
+        testMarks.put("Midsemester_Examination",st.marks.get("Midsemester_Examination"));
+        testMarks.put("Comprehensive_Examination",st.marks.get("Comprehensive_Examination"));
         return testMarks;
     }
-    void createTest(String testName, float weightagePercent , int duration , int noOfQuestions, Course c1,ArrayList<Character> anskey){
+    void createTest(String testName, float weightagePercent , int duration , int noOfQuestions, Course c1,ArrayList<Character> anskey) throws InterruptedException{
         Test t = new Test(testName,weightagePercent ,duration ,noOfQuestions); 
         c1.allTest.add(t); 
-        System.out.println(t.testName+" Test Created by "+ this.name);
+        System.out.print("Creating Test");
+        Student.printWithDelays("...", TimeUnit.MILLISECONDS, 1500); 
+		System.out.println(""); 
+		System.out.println(t.testName+" Test Created by "+ this.name);
         t.setKey(anskey);
     }
 }
 
 class CourseMan {
 
-	public static void main(String[] args) { 
-		Scanner in = new Scanner(System.in);
+	public static void main(String[] args) throws InterruptedException { 
+		Scanner sc = new Scanner(System.in);
 		Course OOP = new Course("Object Oriented Programming",4,5102,3); 
 		ArrayList<String> arl=new ArrayList<>();
 		arl.add("OOP");
@@ -264,7 +284,8 @@ class CourseMan {
 		Teacher AD = new Teacher("Amit Dua", "amit.d12",arl); 
 		Student stud1 = new Student("Harshit","2020A7PS0057P","f20200057","India",OOP); 
 		Student stud2 = new Student("Rachit","2020A7PS0033P","f20200033","India",OOP);
-        System.out.println("Details of the students registerd in the course are: ");
+        System.out.println("Details of the students registered in the course are: ");
+        System.out.print("\n");
 		stud1.viewPersonalDetails();
         System.out.print("\n\n");
         stud2.viewPersonalDetails(); 
@@ -324,30 +345,32 @@ class CourseMan {
 		ansc.add('d'); 
 		AD.createTest("Quiz",30.0f, 1, 10, OOP,ansq); 
 		System.out.println(stud1.name+" is taking a test in the "+OOP.nameCourse+" course."); 
-		stud1.takeTest(OOP); 
+		stud1.takeTest(OOP);
+		System.out.println(stud2.name+" is taking a test in the "+OOP.nameCourse+" course."); 
+		stud2.takeTest(OOP);
 		Student stud3;
 		String name ="",idno="",email="",country=""; 
 		System.out.println("Has any new student joined the course?(Yes/No)");
-		String newreg = in.next();
+		String newreg = sc.next();
 		while(!newreg.equals("No"))
 		{
 			if(newreg.equals("Yes"))
 			{
 				System.out.println("Please enter the name of the student"); 
-				name = in.next(); 
+				name = sc.next(); 
 				System.out.println("Please enter the idno of the student"); 
-				idno = in.next(); 
+				idno = sc.next(); 
 				System.out.println("Please enter the email initials(i.e. till @) of the student"); 
-				email = in.next(); 
+				email = sc.next(); 
 				System.out.println("Please enter the country of the student"); 
-				country = in.next(); 
+				country = sc.next(); 
 				break;
 			} 
 			else
 			{
 				System.out.println("~_~ You had to enter Yes/No"); 
 				System.out.println("Has any new student joined the course?(Yes/No)");
-				newreg = in.next();
+				newreg = sc.next();
 			}
 		}
 		stud3 = new Student(name,idno,email,country,OOP); 
@@ -356,7 +379,7 @@ class CourseMan {
 			System.out.println(stud3.name+ " should take the quiz now.");
 			stud3.takeTest(OOP);
 		}
-		AD.createTest("Midsemester Examination",30.0f, 2, 12, OOP,ansm);
+		AD.createTest("Midsemester_Examination",30.0f, 2, 12, OOP,ansm);
 		System.out.println("Well now is the midsem exams are also available."); 
 		System.out.println("Students have to take the test"); 
 		System.out.println("For "+stud1.name);
@@ -365,20 +388,61 @@ class CourseMan {
 		stud2.takeTest(OOP); 
 		for(int i =0; i<OOP.allTest.size();i++)
 		{
-			if(!OOP.allTest.get(i).testName.equals("Comprehensive Examination"))
+			if(!OOP.allTest.get(i).testName.equals("Comprehensive_Examination"))
 			{
 				OOP.allTest.get(i).testSituation = "Unavailable";
 			}
-		}
-		AD.createTest("Comprehensive Examination",40.0f, 3, 15, OOP,ansc); 
-		System.out.println("The semester end is close by. All the students have to take the commprehensive exam"); 
-		stud1.takeTest(OOP);
-		stud2.takeTest(OOP);
+		} 
+		
 		if(!stud3.name.equals(""))
 		{
-			stud3.takeTest(OOP);
-		}
-		Student.allRegisteredStudents(OOP);
+			int key = 0;
+			while(key!=7)
+			{ 
+				System.out.println("Please enter what you want to do for "+stud3.name);
+				System.out.println("1.View Personal Details!");
+				System.out.println("2.See the marks of Test!");
+				System.out.println("3.See all availale tests!");
+				System.out.println("4. View Enrolled Course.");
+				System.out.println("5. See all the students registered in "+ OOP.nameCourse+" course");
+				System.out.println("6. Take a test");
+				System.out.println("7. Exit"); 
+				key = sc.nextInt(); 
+				switch(key)
+				{
+					case 1: stud3.viewPersonalDetails(); 
+							break; 
+					case 2: {
+								Map<String,Integer> mp3 =new HashMap<>();
+								mp3=AD.viewMarks(stud3);
+								System.out.println(mp3);
+								System.out.print("\n\n");
+							}
+							break;
+					case 3: stud3.availableTests(OOP); 
+							break;
+					case 4: stud3.viewEnrolledCourse(); 
+							break;
+					case 5: Student.allRegisteredStudents(OOP); 
+							break;
+					case 6: stud3.takeTest(OOP); 
+							break;
+					case 7: ; 
+							break;
+					default: System.out.println("Please enter a correct option! ~_~"); 
+							break;
+				}
+				
+			}
+		}  
+		Student.allRegisteredStudents(OOP); 
+		
+		AD.createTest("Comprehensive_Examination",40.0f, 3, 15, OOP,ansc); 
+		System.out.println("The semester end is close by. All the students have to take the commprehensive exam"); 
+		System.out.println("For "+stud1.name);
+		stud1.takeTest(OOP);
+		System.out.println("For "+stud2.name);
+		stud2.takeTest(OOP);
         System.out.print("\n\n"); 
         Map<String,Integer> mp1 =new HashMap<>();
         mp1=AD.viewMarks(stud1);
@@ -394,6 +458,7 @@ class CourseMan {
             mp3=AD.viewMarks(stud3);
             System.out.println(mp3);
             System.out.print("\n\n");
-		}        
+		}
+		System.out.println("Thankyou and have a nice day :-)");       
 	}
 }
